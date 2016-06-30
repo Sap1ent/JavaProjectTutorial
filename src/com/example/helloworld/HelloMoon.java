1b9b430891285a201d88f9ec55b2019d4373036d
Sap1ent <christopher.west@hotmail.co.uk>
2016-06-30 18:12:41 +0100

    committing files from intelllij2

---
+++ src/com/example/helloworld/HelloMoon.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+/**
+ * Created by Sap1ent on 30/06/2016.
+ */
+public class HelloMoon {
+    public static void main(String[] args) {
+        System.out.println("Hello, Moon!");
+
+    }
+}
+}
